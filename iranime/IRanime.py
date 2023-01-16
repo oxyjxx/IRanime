bddb3703d29d9d5a8a89f218d52ddb612af62839
oxyjxx <100666964+oxyjxx@users.noreply.github.com>
2023-01-16 15:48:51 +0200

    Update IRanime.py

--- iranime/IRanime.py
+++ iranime/IRanime.py
@@ -2,4 +2,4 @@ import random
 
 class SenkoPictures():
   
-  senko_url = random.choice(list(open("iranime/urls/senko_pictures.txt")))
+  url = random.choice(list(open("iranime/urls/senko_pictures.txt")))
