172c7f6ff88a00398b24d796b7a1e6de4078f9d0
oxyjxx <100666964+oxyjxx@users.noreply.github.com>
2023-01-16 15:30:24 +0200

    Update IRanime.py

--- iranime/IRanime.py
+++ iranime/IRanime.py
@@ -1,1 +1,12 @@
+import random
 
+class IRanime():
+  
+  @property
+  def senko_pictures(self):
+    try:
+      images = random.choice(list(open("iranime/senko_pictures.txt"))
+      return images
+    except:
+      print("IRanime Error")
+      return None
